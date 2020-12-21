4daf9228fcd949544a0955f561005eb6a2c1ccec
piwiit <bouillaux@hotmail.fr>
2020-12-21 14:20:31 +0100

    add cloudinary picture

--- config/application.rb
+++ config/application.rb
@@ -16,3 +16,13 @@ module Portefolio
     end # the framework and any gems in your application.
   end
 end
+
+ActionMailer::Base.smtp_settings = {
+  user_name: ENV['SENDGRID_USERNAME'],
+  password: ENV['SENDGRID_PASSWORD'],
+  domain: 'yourdomain.com',
+  address: 'smtp.sendgrid.net',
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
