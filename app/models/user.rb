e816518418314d3b91b30bad634b570ea4ad4d26
piwiit <bouillaux@hotmail.fr>
2020-12-21 17:02:12 +0100

    close registration routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ApplicationRecord # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable # Include default devise modules. Others available are:
-  devise :database_authenticatable,
-         :registerable,
-         :recoverable,
-         :rememberable,
-         :validatable
+  devise :database_authenticatable, :recoverable, :rememberable, :validatable
 end
