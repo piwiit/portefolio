0b5d6dd3fdd3f01cbf925b53210399ea1010c4b7
piwiit <bouillaux@hotmail.fr>
2020-12-21 22:18:26 +0100

    update js file

--- app/javascript/packs/js.js
+++
@@ -1,8 +0,0 @@
-let buttonClick = document.querySelector(
-  "body > main > section > div > div > i"
-);
-let userLogin = document.querySelector("body > nav > div > a");
-function onClickUserLogin() {
-  userLogin.classList.toggle("d-none");
-}
-buttonClick.addEventListener("click", onClickUserLogin);
