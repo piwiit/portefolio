1fcc317be27d52d76a5d408d9a78a7a257c4ba8d
piwiit <bouillaux@hotmail.fr>
2020-12-22 15:56:47 +0100

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
 User.destroy_all
+Project.destroy_all
 
 admin = User.new(email: 'bouillaux@hotmail.fr', password: 'azerty')
 admin.save!
+
+discotroc =
+  Project.new(
+    title: 'discotroc ',
+    description:
+      "Vos goûts musicaux ont changé et vos vieux disques ne vous intéressent plus ? Vous pourriez les échanger.C'est exactement ce que nous vous proposons !Discotroc connecte les passionnés de musique et leurs disques à travers le monde. Votre discothèque devient virtuellement illimitée",
+    url: 'https://discotroc.herokuapp.com/'
+  ) #photo = "100_km.png"
+picture =
+  URI.open(
+    'https://res.cloudinary.com/dtevenab3/image/upload/v1608074578/discotroc.png'
+  )
+discotroc.picture.attach(
+  io: picture, filename: 'discotroc.png', content_type: 'image/png'
+)
+discotroc.save!
